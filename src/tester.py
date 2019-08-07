f3a2af06cd11050ab79ce45281bd6f803fadf490
Kendall Jackson <kendallkj00@gmail.com>
2019-08-07 12:52:39 -0400

    Added dataframe test

---
+++ src/tester.py
@@ -0,0 +1,20 @@
+# Testing functionality
+# Kendall Jackson
+
+import requests
+import re
+import unicodecsv as csv
+import pandas as pd
+
+# Reads website column, initializes counter variable
+df = pd.read_csv('./Data/BigBear.csv')
+final_list = []
+email="sample@sample.com"
+
+for index, row in df.iterrows():
+    # if email exists
+    if(index < 10):
+        final_list.append({'business_name': row['business_name'], 'website': row['website'], 'industry': row['industry'], 'city': row['city'], 'state': row['state'], 'email': email })
+            
+
+print(final_list)
